90ce94821f93d41b7a8ee251d78bbded31ba40e9
abschill <ajay198810@gmail.com>
2022-04-13 22:16:14 -0600

    add c version

--- test-cpp/hello.cc
+++ test-cpp/hello.cc
@@ -1,7 +1,7 @@
 #include <nan.h>
 
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
+  info.GetReturnValue().Set(Nan::New("hello from c++").ToLocalChecked());
 }
 
 void Init(v8::Local<v8::Object> exports) {
